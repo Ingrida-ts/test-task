3473b981dc320213aec77454faab82fda42cd0bc
Ingrida Nanieva <Nanieva2008Ingrida@gmail.com>
2024-08-05 21:02:56 +0300

    fhird commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,41 @@
+function ShowNumber(clicked_object) {
+   let ThisNumber=clicked_object.getAttribute('value');
+   let input=document.getElementById('result');
+   if (input.value==="0") {
+    input.value=ThisNumber;
+} else {
+        input.value+=ThisNumber;  
+    }
+    }
+
+
+    function ShowMuth(clicked_object){
+        let ThisMuth=clicked_object.getAttribute('value');
+        let input=document.getElementById('result');
+        let Last=document.getElementById('result').value.slice(-1);
+        
+      if (Last==="+"||Last==="-"||Last==="÷"||Last==="×"){
+           input.value=input.value.slice(0,-1)+ThisMuth;
+    } else{
+        input.value+=ThisMuth;
+    }
+    }
 
 
  function Clear() {
             let input=document.getElementById('result');
-            input.value= " ";
-            
+            input.value= "0";  
         }
 
+        
 function calculateResult() {
-    let input=document.getElementById('result')
+    let input=document.getElementById('result').value.replace(/×/g, '*');
+     Result=input.replace(/÷/g, '/');
+
     try {
-        input.value = eval(input.value);
+        document.getElementById('result').value = eval(Result);
     } 
     catch (error) {
-        input.value = 'Error';
+        document.getElementById('result').value = 'Error';
     }
 }
-
